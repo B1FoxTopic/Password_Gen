7c26f02b25358c60cc81feac32d403d3c0b654a5
FoxTopic <B1FoxTopic@gmail.com>
2023-02-07 23:51:51 +0000

    #

--- Gen.py
+++ Gen.py
@@ -4,7 +4,9 @@ import string
 
 app = Flask(__name__)
 
-def generate_password(min_length=10, max_length=16, blacklist=""):
+def generate_password(min_length=10, max_length=16, blacklist=None):
+    if blacklist is None:
+        blacklist = "{ , } , ] , [ , ~ , | , ( , ) , ` , ; , :"
     length = random.randint(min_length, max_length)
     characters = "".join(c for c in string.ascii_letters + string.digits + string.punctuation if c not in blacklist)
     password = ''.join(random.choice(characters) for i in range(length))
