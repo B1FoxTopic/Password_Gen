2fba243d9ba188bb1e85d5e8dfaec4c470014d91
FoxTopic <B1FoxTopic@gmail.com>
2023-02-08 00:44:22 +0000

    In "style.css" added some color to the website so its easier for me to look at lol. Also got a cool temp button from https://getcssscan.com/.
    In “Gen.py” fixed some lines + added a password hash from hashlib using sha256 for now.
    Added text on HTML file to display the Hash from the password above tba.
    
    In “index.html” added a Hash: {{ password_hash }} so I could display the hash for later.

--- Gen.py
+++ Gen.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, render_template
 import random
 import string
+import hashlib
 
 app = Flask(__name__)
 
 def generate_password(min_length=10, max_length=16, blacklist=None):
     if blacklist is None:
-        blacklist = "{ , } , ] , [ , ~ , | , ( , ) , ` , ; , :"
+        blacklist = "{,},],[,~,|,(,),`,;,:,.,<,>,\,/"
     length = random.randint(min_length, max_length)
     characters = "".join(c for c in string.ascii_letters + string.digits + string.punctuation if c not in blacklist)
     password = ''.join(random.choice(characters) for i in range(length))
@@ -15,10 +16,12 @@ def generate_password(min_length=10, max_length=16, blacklist=None):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     password = generate_password()
+    password_hash = hashlib.sha256(password.encode()).hexdigest()
     if request.method == 'POST':
         blacklist = request.form.get('blacklist')
         password = generate_password(blacklist=blacklist)
-    return render_template('index.html', password=password)
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+    return render_template('index.html', password=password, password_hash=password_hash)
 
 if __name__ == '__main__':
     app.run()
